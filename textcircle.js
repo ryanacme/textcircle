d3cb9a4c02f4fc70edfc6fd78ac540393b1b8bd7
Ryan Braving <ryan.braving@gmail.com>
2017-02-15 16:28:02 +1300

    week1-TextCircle

--- textcircle.js
+++ textcircle.js
@@ -1,9 +1,30 @@
-if (Meteor.isClient){
+this.Documents = new Mongo.Collection("documents");
 
+if (Meteor.isClient){
+	Template.editor.helpers({
+		docid: function(){
+			console.log("hiiiiiiiiiiiiiiiiiiii");
+			var doc = Documents.findOne();
+			if (doc){
+				return doc._id;
+			}
+			else{
+				return undefined;
+			}
+			// console.log(Documents.findOne());
+			// return Documents.findOne()._id;
+
+		}
+
+	});
+	
 }
 
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
+		if (!Documents.findOne()){
+			Documents.insert({title:"my first doc"});
+		}
 	})
 }
